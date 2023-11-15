0f92a99134371b1cfe259ab9d8882a4c901e9e78
bnbnac <bnbnac@naver.com>
2023-11-15 09:58:28 +0900

    init

---
+++ src/main/java/com/tftad/TftadApplication.java
@@ -0,0 +1,13 @@
+package com.tftad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftadApplication.class, args);
+	}
+
+}
