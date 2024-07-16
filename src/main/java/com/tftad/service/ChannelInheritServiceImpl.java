016e11bd63cc694d2c80ad8965523ba17f837fb8
bnbnac <bnbnac@naver.com>
2024-07-16 15:48:16 +0900

    feat: apply jpql bulk update when the number of posts to inherit is over 1000

--- src/main/java/com/tftad/service/ChannelInheritServiceImpl.java
+++ src/main/java/com/tftad/service/ChannelInheritServiceImpl.java
@@ -2,8 +2,11 @@ package com.tftad.service;
 
 import com.tftad.domain.Post;
 import com.tftad.repository.PostRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,13 +14,31 @@ import java.util.List;
 @Service
 public class ChannelInheritServiceImpl implements ChannelInheritService {
 
+    @PersistenceContext
+    private final EntityManager entityManager;
     private final PostRepository postRepository;
 
     @Override
+    @Transactional
     public void inheritPostsOfChannel(Long channelId, Long memberId) {
         List<Post> posts = postRepository.findByChannelId(channelId);
+
+        if (posts.size() > 1000) {
+            bulkInheritPostsOfChannel(channelId, memberId);
+            return;
+        }
+
         for (Post post : posts) {
             post.inherit(memberId);
         }
+    }
+
+    private void bulkInheritPostsOfChannel(Long channelId, Long memberId) {
+        String jpql = "UPDATE Post p SET p.memberId = :memberId WHERE p.channelId = :channelId";
+
+        entityManager.createQuery(jpql)
+                .setParameter("memberId", memberId)
+                .setParameter("channelId", channelId)
+                .executeUpdate();
     }
 }
