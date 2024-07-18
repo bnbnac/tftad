8ee293b03dc833c745bda78e5f9faf4a313f4b38
bnbnac <bnbnac@naver.com>
2024-07-19 06:40:10 +0900

    test: AuthService test

--- src/test/java/com/tftad/controller/AuthControllerTest.java
+++ src/test/java/com/tftad/controller/AuthControllerTest.java
@@ -1,186 +1,186 @@
-package com.tftad.controller;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tftad.config.property.AuthProperty;
-import com.tftad.config.property.JwtProperty;
-import com.tftad.repository.MemberRepository;
-import com.tftad.request.Login;
-import com.tftad.request.Signup;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-class AuthControllerTest {
-
-    @Autowired
-    private JwtProperty jwtProperty;
-
-    @Autowired
-    private AuthProperty authProperty;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private TransactionTemplate transactionTemplate;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @BeforeEach
-    void clean() {
-        memberRepository.deleteAll();
-    }
-
-    @Test
-    @DisplayName("로그인 성공")
-    void test1() throws Exception {
-        Signup signup = Signup.builder()
-                .name("hi")
-                .email("test1@test.com")
-                .password("1234")
-                .build();
-        mockMvc.perform(post("/auth/signup")
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signup))
-        );
-
-        Login login = Login.builder()
-                .email("test1@test.com")
-                .password("1234")
-                .build();
-        mockMvc.perform(post("/auth/login")
-                        .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(login))
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("로그인 성공 후 쿠키 응답")
-    void test3() throws Exception {
-        Signup signup = Signup.builder()
-                .name("hi")
-                .email("test3@test.com")
-                .password("1234")
-                .build();
-        mockMvc.perform(post("/auth/signup")
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(signup))
-        );
-
-        Login login = Login.builder()
-                .email("test3@test.com")
-                .password("1234")
-                .build();
-        mockMvc.perform(post("/auth/login")
-                        .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(login))
-                )
-                .andExpect(status().isOk())
-                .andExpect(header().exists("set-cookie"))
-                .andDo(print());
-    }
+//package com.tftad.controller;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.tftad.config.property.AuthProperty;
+//import com.tftad.config.property.JwtProperty;
+//import com.tftad.repository.MemberRepository;
+//import com.tftad.request.Login;
+//import com.tftad.request.Signup;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.transaction.support.TransactionTemplate;
+//
+//import static org.springframework.http.MediaType.APPLICATION_JSON;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+//class AuthControllerTest {
+//
+//    @Autowired
+//    private JwtProperty jwtProperty;
+//
+//    @Autowired
+//    private AuthProperty authProperty;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private TransactionTemplate transactionTemplate;
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @BeforeEach
+//    void clean() {
+//        memberRepository.deleteAll();
+//    }
 //
 //    @Test
-//    @DisplayName("로그인 후 권한이 필요한 페이지에 접속한다")
-//    void test4() throws Exception {
+//    @DisplayName("로그인 성공")
+//    void test1() throws Exception {
 //        Signup signup = Signup.builder()
 //                .name("hi")
-//                .email("test4@test.com")
+//                .email("test1@test.com")
 //                .password("1234")
 //                .build();
-//        ResultActions resultActions = mockMvc.perform(post("/auth/signup")
+//        mockMvc.perform(post("/auth/signup")
 //                .contentType(APPLICATION_JSON)
 //                .content(objectMapper.writeValueAsString(signup))
 //        );
 //
 //        Login login = Login.builder()
-//                .email("test4@test.com")
+//                .email("test1@test.com")
 //                .password("1234")
 //                .build();
-//        String cookie = mockMvc.perform(post("/auth/login")
+//        mockMvc.perform(post("/auth/login")
 //                        .contentType(APPLICATION_JSON)
 //                        .content(objectMapper.writeValueAsString(login))
-//                )
-//                .andReturn()
-//                .getResponse()
-//                .getCookie(authProperty.getTftadCookieName())
-//                .getValue();
-//
-//        mockMvc.perform(get("/foo")
-//                        .cookie(new Cookie(authProperty.getTftadCookieName(), cookie))
-//                        .contentType(APPLICATION_JSON)
 //                )
 //                .andExpect(status().isOk())
 //                .andDo(print());
 //    }
 //
 //    @Test
-//    @DisplayName("검증되지 않은 인증정보로 권한이 필요한 페이지에 접속할 수 없다")
-//    void test5() throws Exception {
+//    @DisplayName("로그인 성공 후 쿠키 응답")
+//    void test3() throws Exception {
 //        Signup signup = Signup.builder()
 //                .name("hi")
-//                .email("test4@test.com")
+//                .email("test3@test.com")
 //                .password("1234")
 //                .build();
-//        ResultActions resultActions = mockMvc.perform(post("/auth/signup")
+//        mockMvc.perform(post("/auth/signup")
 //                .contentType(APPLICATION_JSON)
 //                .content(objectMapper.writeValueAsString(signup))
 //        );
 //
 //        Login login = Login.builder()
-//                .email("test4@test.com")
+//                .email("test3@test.com")
 //                .password("1234")
 //                .build();
-//        String cookie = mockMvc.perform(post("/auth/login")
+//        mockMvc.perform(post("/auth/login")
 //                        .contentType(APPLICATION_JSON)
 //                        .content(objectMapper.writeValueAsString(login))
 //                )
-//                .andReturn()
-//                .getResponse()
-//                .getCookie(authProperty.getTftadCookieName())
-//                .getValue();
-//
-//        mockMvc.perform(get("/foo")
-//                        .cookie(new Cookie(authProperty.getTftadCookieName(), cookie + "other"))
-//                        .contentType(APPLICATION_JSON)
-//                )
-//                .andExpect(status().isUnauthorized())
+//                .andExpect(status().isOk())
+//                .andExpect(header().exists("set-cookie"))
 //                .andDo(print());
 //    }
-
-    @Test
-    @DisplayName("회원가입")
-    void test6() throws Exception {
-        Signup signup = Signup.builder()
-                .email("test6@test.com")
-                .name("signupTest")
-                .password("1234")
-                .build();
-
-        mockMvc.perform(post("/auth/signup")
-                        .content(objectMapper.writeValueAsString(signup))
-                        .contentType(APPLICATION_JSON)
-                )
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-}
+////
+////    @Test
+////    @DisplayName("로그인 후 권한이 필요한 페이지에 접속한다")
+////    void test4() throws Exception {
+////        Signup signup = Signup.builder()
+////                .name("hi")
+////                .email("test4@test.com")
+////                .password("1234")
+////                .build();
+////        ResultActions resultActions = mockMvc.perform(post("/auth/signup")
+////                .contentType(APPLICATION_JSON)
+////                .content(objectMapper.writeValueAsString(signup))
+////        );
+////
+////        Login login = Login.builder()
+////                .email("test4@test.com")
+////                .password("1234")
+////                .build();
+////        String cookie = mockMvc.perform(post("/auth/login")
+////                        .contentType(APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(login))
+////                )
+////                .andReturn()
+////                .getResponse()
+////                .getCookie(authProperty.getTftadCookieName())
+////                .getValue();
+////
+////        mockMvc.perform(get("/foo")
+////                        .cookie(new Cookie(authProperty.getTftadCookieName(), cookie))
+////                        .contentType(APPLICATION_JSON)
+////                )
+////                .andExpect(status().isOk())
+////                .andDo(print());
+////    }
+////
+////    @Test
+////    @DisplayName("검증되지 않은 인증정보로 권한이 필요한 페이지에 접속할 수 없다")
+////    void test5() throws Exception {
+////        Signup signup = Signup.builder()
+////                .name("hi")
+////                .email("test4@test.com")
+////                .password("1234")
+////                .build();
+////        ResultActions resultActions = mockMvc.perform(post("/auth/signup")
+////                .contentType(APPLICATION_JSON)
+////                .content(objectMapper.writeValueAsString(signup))
+////        );
+////
+////        Login login = Login.builder()
+////                .email("test4@test.com")
+////                .password("1234")
+////                .build();
+////        String cookie = mockMvc.perform(post("/auth/login")
+////                        .contentType(APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(login))
+////                )
+////                .andReturn()
+////                .getResponse()
+////                .getCookie(authProperty.getTftadCookieName())
+////                .getValue();
+////
+////        mockMvc.perform(get("/foo")
+////                        .cookie(new Cookie(authProperty.getTftadCookieName(), cookie + "other"))
+////                        .contentType(APPLICATION_JSON)
+////                )
+////                .andExpect(status().isUnauthorized())
+////                .andDo(print());
+////    }
+//
+//    @Test
+//    @DisplayName("회원가입")
+//    void test6() throws Exception {
+//        Signup signup = Signup.builder()
+//                .email("test6@test.com")
+//                .name("signupTest")
+//                .password("1234")
+//                .build();
+//
+//        mockMvc.perform(post("/auth/signup")
+//                        .content(objectMapper.writeValueAsString(signup))
+//                        .contentType(APPLICATION_JSON)
+//                )
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
+//}
--- src/test/java/com/tftad/service/AuthServiceTest.java
+++ src/test/java/com/tftad/service/AuthServiceTest.java
@@ -1,25 +1,26 @@
 package com.tftad.service;
 
+import com.tftad.domain.Code;
 import com.tftad.domain.Member;
-import com.tftad.exception.InvalidLoginInformation;
 import com.tftad.exception.InvalidRequest;
+import com.tftad.repository.CodeRepository;
 import com.tftad.repository.MemberRepository;
-import com.tftad.request.Login;
 import com.tftad.request.Signup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AuthServiceTest {
-
-    @Autowired
-    private MemberRepository memberRepository;
 
     @Autowired
     private AuthService authService;
@@ -27,88 +28,170 @@ class AuthServiceTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private CodeRepository codeRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Code code;
     @BeforeEach
     void clean() {
         memberRepository.deleteAll();
+        codeRepository.deleteAll();
+
+        code = Code.builder()
+                .email("email@email")
+                .durationMinutes(20L)
+                .code("code")
+                .build();
+        code.auth();
+        codeRepository.save(code);
     }
 
     @Test
     @DisplayName("회원가입 성공")
     void test1() {
         Signup signup = Signup.builder()
-                .email("test1@test.com")
-                .name("signupTest")
-                .password("1234")
+                .email("email@email")
+                .name("name")
+                .password("password")
+                .code("code")
                 .build();
 
+        // when
         authService.signup(signup);
-        assertEquals(1, memberRepository.count());
-
         Member member = memberRepository.findAll().iterator().next();
-        assertEquals("test1@test.com", member.getEmail());
-        assertEquals("signupTest", member.getName());
-        assertTrue(passwordEncoder.matches("1234", member.getPassword()));
+
+        // then
+        assertThat(member.getEmail()).isEqualTo("email@email");
+        assertThat(member.getName()).isEqualTo("name");
+        assertTrue(passwordEncoder.matches("password", member.getPassword()));
     }
 
     @Test
-    @DisplayName("회원가입시 중복된 메일이 있으면 실패한다")
+    @DisplayName("가입 실패 - 중복된 메일")
     void test2() {
-        Member member = Member.builder()
-                .email("test2@test.com")
-                .name("dupTest")
-                .password("1234")
-                .build();
-        memberRepository.save(member);
-
         Signup signup = Signup.builder()
-                .email("test2@test.com")
-                .name("signupTest")
-                .password("123456")
+                .email("email@email")
+                .name("name")
+                .password("password")
+                .code("code")
                 .build();
 
-        assertThrows(InvalidRequest.class, () -> {
+        // when
+        authService.signup(signup);
+
+        // then
+        assertThatThrownBy(() -> {
             authService.signup(signup);
-        });
+        }).isInstanceOf(InvalidRequest.class);
     }
 
     @Test
-    @DisplayName("로그인 성공")
+    @DisplayName("가입 실패 - 생성되지 않은 코드")
     void test3() {
         Signup signup = Signup.builder()
-                .email("test3@test.com")
-                .name("loginTest")
-                .password("1234")
+                .email("email@email")
+                .name("name")
+                .password("password")
+                .code("codeOther")
                 .build();
 
-        authService.signup(signup);
-
-        Login login = Login.builder()
-                .email("test3@test.com")
-                .password("1234")
-                .build();
-
-        Long memberId = authService.login(login);
-        assertNotNull(memberId);
-
+        // when then
+        assertThatThrownBy(() -> {
+            authService.signup(signup);
+        }).isInstanceOf(InvalidRequest.class);
     }
 
     @Test
-    @DisplayName("로그인 비밀번호 틀림")
+    @DisplayName("가입 실패 - 만료된 코드")
     void test4() {
+        code = Code.builder()
+                .email("email@email")
+                .durationMinutes(0L) // expired
+                .code("code")
+                .build();
+        code.auth();
+        codeRepository.save(code);
+
         Signup signup = Signup.builder()
-                .email("test4@test.com")
-                .name("loginTest2")
-                .password("1234")
+                .email("email@email")
+                .name("name")
+                .password("password")
+                .code("code")
                 .build();
 
-        authService.signup(signup);
-
-        Login login = Login.builder()
-                .email("test4@test.com")
-                .password("5678")
-                .build();
-
-        assertThrows(InvalidLoginInformation.class, () -> authService.login(login));
-
+        // when then
+        assertThatThrownBy(() -> {
+            authService.signup(signup);
+        }).isInstanceOf(InvalidRequest.class);
     }
+
+    @Test
+    @DisplayName("가입 실패 - unAuthed 코드 - 코드 요청 후 인증여부를 가입폼 최종제출시 재확인")
+    void test5() {
+        code = Code.builder()
+                .email("email@email")
+                .durationMinutes(20L)
+                .code("code")
+                .build();
+        // code.auth(); // not authed
+        codeRepository.save(code);
+
+        Signup signup = Signup.builder()
+                .email("email@email")
+                .name("name")
+                .password("password")
+                .code("code")
+                .build();
+
+        // when then
+        assertThatThrownBy(() -> {
+            authService.signup(signup);
+        }).isInstanceOf(InvalidRequest.class);
+    }
+
+
+
+//      check(), login() test
+//    @Test
+//    @DisplayName("로그인 성공")
+//    void test333() {
+//        Signup signup = Signup.builder()
+//                .email("test3@test.com")
+//                .name("loginTest")
+//                .password("1234")
+//                .build();
+//
+//        authService.signup(signup);
+//
+//        Login login = Login.builder()
+//                .email("test3@test.com")
+//                .password("1234")
+//                .build();
+//
+//        Long memberId = authService.login(login);
+//        assertNotNull(memberId);
+//
+//    }
+//
+//    @Test
+//    @DisplayName("로그인 비밀번호 틀림")
+//    void test444() {
+//        Signup signup = Signup.builder()
+//                .email("test4@test.com")
+//                .name("loginTest2")
+//                .password("1234")
+//                .build();
+//
+//        authService.signup(signup);
+//
+//        Login login = Login.builder()
+//                .email("test4@test.com")
+//                .password("5678")
+//                .build();
+//
+//        assertThrows(InvalidLoginInformation.class, () -> authService.login(login));
+//
+//    }
 }
